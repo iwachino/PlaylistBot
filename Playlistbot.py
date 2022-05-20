152a739dc3af2d166869b29765ed75b2c11e19b4
seajaysec <7016191+seajaysec@users.noreply.github.com>
2022-05-20 17:11:41 -0400

    May 20, 2022 5:11 PM

--- Playlistbot.py
+++ Playlistbot.py
@@ -27,5 +27,5 @@ with open("./%s.csv" % title, "w") as f:
     for (artists, songs) in playlist:
         f.write("{0},{1}\n".format(artists, songs))
 
-print(list(playlist))
+print(list(range(playlist)))
 print(f"file {title}.csv successfully created!")
